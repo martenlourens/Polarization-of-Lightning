6bf505ea521fdf9bda81d2f6f4426bab4715babf
Marten Lourens <lourens@astro.rug.nl>
2020-05-01 18:36:15 +0200

    Made minor changes to pickleIO

--- pickleIO/pickle_clip_to_file.py
+++ pickleIO/pickle_clip_to_file.py
@@ -8,6 +8,7 @@ from LoLIM.utilities import processed_data_dir
 
 timeID = "D20190424T210306.154Z"
 utilities.default_processed_data_loc = "/home/student4/Marten/processed_files"
+processed_data_folder = processed_data_dir(timeID)
 
 ui = tk.Tk()
 ui.withdraw()
@@ -16,5 +17,5 @@ ui.destroy()
 
 data = pickle.loads(clip)
 
-with open(processed_data_dir(timeID) + "/polarization_data/pulseIDs.pkl", 'wb') as f:
+with open(processed_data_folder + "/polarization_data/pulseIDs.pkl", 'wb') as f:
 	pickle.dump(data,f,protocol=pickle.HIGHEST_PROTOCOL)
--- pickleIO/save_source_info.py
+++ pickleIO/save_source_info.py
@@ -4,17 +4,17 @@ from LoLIM.iterativeMapper.iterative_mapper import read_header
 from LoLIM import utilities
 from LoLIM.utilities import processed_data_dir
 
-#import numpy as np
 import pickle
 import json
 
 timeID = "D20190424T210306.154Z"
 utilities.default_processed_data_loc = "/home/student4/Marten/processed_files"
+processed_data_folder = processed_data_dir(timeID)
 
-header = read_header( 'iterMapper_50_CS002',  timeID)
-data = header.load_data_as_sources(maxRMS=2.5E-9)
+header = read_header('iterMapper_50_CS002',  timeID)
+data = header.load_data_as_sources(maxRMS=2.5E-9) #DON'T FORGET TO CHECK THIS!!!
 
-with open(processed_data_dir(timeID)+"/polarization_data/pulseIDs.pkl", 'rb') as f:
+with open(processed_data_folder+"/polarization_data/pulseIDs.pkl", 'rb') as f:
 	pulseIDs = pickle.load(f)
 
 
@@ -26,5 +26,5 @@ for info in data:
 			sourceDict["{}".format(info.uniqueID)] = {'XYZT':info.XYZT.tolist(), 'covXYZ':info.cov_matrix.tolist(), 'rmsT':info.RMS}
 			break
 
-with open(processed_data_dir(timeID)+"/polarization_data/source_info.json", 'w') as f:
+with open(processed_data_folder+"/polarization_data/source_info.json", 'w') as f:
 	json.dump(sourceDict,f)
